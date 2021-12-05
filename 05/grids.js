d7787a842b044ae7e61e77640a4a12763dd5a818
Rudy Pelrine <drawncloser@gmail.com>
2021-12-05 13:45:42 -0600

    optimize

--- 05/grids.js
+++ 05/grids.js
@@ -1,28 +1,24 @@
 const { makeGrid, range, vectorType, IS_X, IS_Y, IS_DIAG } = require('./util');
 
-const markXRange = (grid, y, x1, x2) => {
+const markXRange = (grid, { y1, x1, x2 }) => {
     const xRange = range(x1, x2);
 
     for (let i in xRange) {
         const x = xRange[i];
-        grid[x][y] = grid[x][y] + 1;
+        grid[x][y1] = grid[x][y1] + 1;
     }
-
-    return grid;
 };
 
-const markYRange = (grid, x, y1, y2) => {
+const markYRange = (grid, { x1, y1, y2 }) => {
     const yRange = range(y1, y2);
 
     for (let i in yRange) {
         const y = yRange[i];
-        grid[x][y] = grid[x][y] + 1;
+        grid[x1][y] = grid[x1][y] + 1;
     }
-
-    return grid;
 };
 
-const markDiagRange = (grid, x1, x2, y1, y2) => {
+const markDiagRange = (grid, { x1, x2, y1, y2 }) => {
     const xDir = x2 > x1 ? 1 : -1;
     const yDir = y2 > y1 ? 1 : -1;
     const xRange = range(x1, x2);
@@ -33,32 +29,19 @@ const markDiagRange = (grid, x1, x2, y1, y2) => {
 
         grid[x][y] = grid[x][y] + 1;
     }
-
-    return grid;
 };
 
-exports.calculateIntersectionGrid = (vectors, calulateDiagonals = false) => {
-    let grid = makeGrid(vectors);
+exports.calculateIntersectionGrid = (vectors, calculateDiagonals = false) => {
+    const grid = makeGrid(vectors);
 
     for (let v in vectors) {
-        const vType = vectorType(vectors[v]);
-        const { x1, y1, x2, y2 } = vectors[v];
+        const vector = vectors[v];
+        const vType = vectorType(vector);
 
-        switch (vType) {
-            case IS_X: {
-                grid = markYRange(grid, x1, y1, y2);
-                break;
-            }
-            case IS_Y: {
-                grid = markXRange(grid, y1, x1, x2);
-                break;
-            }
-            case IS_DIAG: {
-                if (calulateDiagonals)
-                    grid = markDiagRange(grid, x1, x2, y1, y2);
-                break;
-            }
-        }
+        if (vType === IS_X) markYRange(grid, vector);
+        else if (vType === IS_Y) markXRange(grid, vector);
+        else if (calculateDiagonals && vType === IS_DIAG)
+            markDiagRange(grid, vector);
     }
 
     const intersectedArea = grid.reduce((area, row) => {
