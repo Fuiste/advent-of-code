8c24a4a3c2bcb4a0510c91571bb37c9e9912a62c
Rudy Pelrine <drawncloser@gmail.com>
2021-12-06 11:15:42 -0500

    Newlines

--- scripts/run-solutions.sh
+++ scripts/run-solutions.sh
@@ -5,7 +5,10 @@ for i in "01" "02" "03" "04" "05" "06";
 		cd ./$i;
 		echo "Solution 1:"
 		node solution-1.js
+		echo ""
 		echo "Solution 2:"
 		node solution-2.js
+		echo ""
+		echo ""
 		cd ..
 	done
