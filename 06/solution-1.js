ae02e00182382bf15c64b7280bc6564c182ef37c
Rudy Pelrine <drawncloser@gmail.com>
2021-12-06 11:12:24 -0500

    Cleanup

--- 06/solution-1.js
+++ 06/solution-1.js
@@ -3,17 +3,21 @@ const { getNextDay, getTotal } = require('./fish');
 const { getInputs, getFileData, toInputMap } = require('./util');
 
 const NUM_DAYS = 80;
-const data = getFileData('input.txt');
-const inputs = getInputs(data);
 
-let inputMap = toInputMap(inputs);
-for (let _ in [...Array(NUM_DAYS).keys()]) {
-    inputMap = getNextDay(inputMap);
-}
+const runSim = () => {
+    const data = getFileData('input.txt');
+    const inputs = getInputs(data);
 
+    let inputMap = toInputMap(inputs);
+    for (let _ in [...Array(NUM_DAYS).keys()]) {
+        inputMap = getNextDay(inputMap);
+    }
+
+    return getTotal(inputMap);
+};
+
+const total = runSim();
 const after = new Date().getTime();
 console.log(
-    `Lanternfish after ${NUM_DAYS} days: ${getTotal(inputMap)} (${
-        after - before
-    } ms)`
+    `Lanternfish after ${NUM_DAYS} days: ${total} (${after - before} ms)`
 );
